4eb0425949fe89c4311daff20baadcc028cd6f04
Andi Altendorfer <andi@iboard.cc>
2011-05-31 09:12:09 +0200

    Initial commit

---
+++ app/controllers/test_controller_controller.rb
@@ -0,0 +1,5 @@
+class TestControllerController < ApplicationController
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c21996e2dead959fd5cabf9790a9854fbbe2f9ba596e7899c827bf2cefaaa4840604c147bfc9b1681735092b61efbb7118ba99a6465436cb5e71e3d5969715b1'
